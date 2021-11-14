0a3b91d8432de0ca5144a9da422ba07cc86b1fcf
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2021-11-14 12:50:30 +0100

    Upload Console App Project skeleton

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DataStructuresAndAlgorithms
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
