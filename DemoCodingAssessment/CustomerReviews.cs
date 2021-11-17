fc089eea42cd986b7eed1440c7653061038daaed
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2021-11-17 14:48:46 +0100

    Customer Reviews class now contain only relevant code

--- DemoCodingAssessment/CustomerReviews.cs
+++ DemoCodingAssessment/CustomerReviews.cs
@@ -3,45 +3,13 @@
     using System;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.IO;
-    using System.Linq;
 
     /// <summary>
     /// Question 1 ( number one)
     /// </summary>
     public class CustomerReviews
     {
-        public void GetSuggestions()
-        {
-            TextWriter textWriter = new StreamWriter("C:\\Tempo\\note.txt", true);
-
-            //int repositoryCount = Convert.ToInt32(Console.ReadLine()?.Trim());
-
-            //List<string> repository = new List<string>();
-
-            //for (int i = 0; i < repositoryCount; i++)
-            //{
-            //    string repositoryItem = Console.ReadLine();
-            //    repository.Add(repositoryItem);
-            //}
-
-            //string customerQuery = Console.ReadLine();
-            List<string> repository = new() { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
-            List<string> customerQueries = new List<string> { "mo", "mou", "mous", "mouse" };
-
-            List<List<string>> result = new List<List<string>>();
-            foreach (string customerQuery in customerQueries)
-            {
-                List<string> resultPerTime = this.SearchSuggestions(repository, customerQuery);
-                result.Add(resultPerTime);
-            }
-
-            textWriter.WriteLine(string.Join("\n", result.Select(list => string.Join(" ", list))));
-            textWriter.Flush();
-            textWriter.Close();
-        }
-
-        private List<string> SearchSuggestions(List<string> repository, string customerQuery)
+        public List<string> SearchSuggestions(List<string> repository, string customerQuery)
         {
             List<string> suggestionList = new List<string>();
             if (string.IsNullOrWhiteSpace(customerQuery) || customerQuery.Length < 2)
--- Program.cs
+++ Program.cs
@@ -1,13 +1,49 @@
-﻿using DataStructuresAndAlgorithms.DemoCodingAssessment;
-
-namespace DataStructuresAndAlgorithms
+﻿namespace DataStructuresAndAlgorithms
 {
+    using DemoCodingAssessment;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
 
     class Program
     {
         static void Main()
+        {
+            CheckIfOrderWins();
+        }
+
+        public static void GetSuggestions()
+        {
+            TextWriter textWriter = new StreamWriter("C:\\Tempo\\note.txt", true);
+
+            //int repositoryCount = Convert.ToInt32(Console.ReadLine()?.Trim());
+
+            //List<string> repository = new List<string>();
+
+            //for (int i = 0; i < repositoryCount; i++)
+            //{
+            //    string repositoryItem = Console.ReadLine();
+            //    repository.Add(repositoryItem);
+            //}
+
+            //string customerQuery = Console.ReadLine();
+            List<string> repository = new() { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
+            List<string> customerQueries = new List<string> { "mo", "mou", "mous", "mouse" };
+
+            List<List<string>> result = new List<List<string>>();
+            var customerReviews = new CustomerReviews();
+            foreach (string customerQuery in customerQueries)
+            {
+                List<string> resultPerTime = customerReviews.SearchSuggestions(repository, customerQuery);
+                result.Add(resultPerTime);
+            }
+
+            textWriter.WriteLine(string.Join("\n", result.Select(list => string.Join(" ", list))));
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        public static void CheckIfOrderWins()
         {
             /*
              * First test data - Working
@@ -38,11 +74,12 @@ namespace DataStructuresAndAlgorithms
 
             var codePromo = new CodePromo();
             int result = codePromo.WeHaveWinner(codeList, shoppingCart);
-            
+
             TextWriter textWriter = new StreamWriter("C:\\Tempo\\note.txt", true);
             textWriter.WriteLine(result);
             textWriter.Flush();
             textWriter.Close();
         }
+
     }
 }
